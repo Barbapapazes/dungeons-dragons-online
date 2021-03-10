ba9cf697c9b86c05f7dbcbe5f4ee206aae2a87b5
Makelonn <maelie.lebaron@gmail.com>
2021-03-11 00:24:12 +0100

    feature/map : basic map function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame as pg
 from src.Game import Game
 import signal
 
+from src.Map import Map
+
 pg.init()
 pg.mouse.set_cursor(*pg.cursors.tri_left)
 g = Game()
@@ -11,3 +13,5 @@ signal.signal(signal.SIGINT, signal.SIG_IGN)
 while g.running:
     g.current_menu.display_menu()
     g.game_loop()
+
+mymap = Map("src/maps/maptest/maptest.txt")
