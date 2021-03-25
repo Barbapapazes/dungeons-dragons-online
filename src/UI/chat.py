9e47a4428149933a868bd655a3267d25224d1bb6
enzo-hmdy <enzo.hmdy@gmail.com>
2021-03-25 20:24:38 +0100

    chat_in_game : /rename command and display smoother

--- src/UI/chat.py
+++ src/UI/chat.py
@@ -11,7 +11,7 @@ class Chat:
     def __init__(self, width, height, position, text_color, font_size, game):
         self.log = Log(width, height, position, text_color, font_size, game)
         self.my_inputbox = Input(
-            game, position[1] + width // 2, position[0] + height + 50, width, 50, path.join(fonts_folder, 'alegreya.ttf'), font_size, max_length=140)
+            game, position[1] + width // 2, position[0] + height + 50, width, 50, path.join(fonts_folder, 'ShareTechMono-Regular.ttf'), font_size, max_length=(width // font_size))
         self.user_text = ""
         self.is_send = False
         self.text_to_send = ""
@@ -27,13 +27,28 @@ class Chat:
 
         if event.type == pg.KEYDOWN:
 
-            if event.key == pg.K_RETURN and self.my_inputbox.get_text() != "" and not self.is_send:
+            if event.key == pg.K_RETURN and self.my_inputbox.get_text() != "":
                 self.user_text = self.my_inputbox.get_text()
-                self.log.add_log(self.name + " : " + self.user_text)
+                if self.user_text[0] == "/":
+                    first_word = self.user_text.split()[0]
+                    second_word = self.user_text.split()[1]
+                    if first_word == "/rename":
+                        self.name = second_word
+
+                if self.user_text[0] != "/":
+                    self.log.add_log(self.name + " : " + self.user_text)
                 self.my_inputbox.flush_text()
 
         self.my_inputbox.handle_events(event)
+        self.log.event(event)
 
     def draw(self, surface):
         self.log.print_log(surface)
         self.my_inputbox.display_box()
+
+    def commands(self):
+        if self.user_text[0] == "/":
+            first_word = self.user_text.split()[0]
+            second_word = self.user_text.split()[1]
+            if first_word == "/rename":
+                self.name = second_word
