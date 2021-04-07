69c0193a715bee04dad47bff9b6963f46b23e94c
Enzo-hmdy <78434618+Enzo-hmdy@users.noreply.github.com>
2021-04-07 16:30:00 +0200

    Chat in game (#10)
    * Chat_in_game : Add Log.py file
    
    * color.py : Add grey codex
    
    * Chat_in_game : Creation of chat class in progress
    
    * chat in game : creation of UI folder
    
    * chat_in_game : creation of UI folder and all files needed
    
    * chat_in_game : chat is printed but doen't work yet
    
    * chat_in game : Chat windows is well positioned
    
    * Chat_in_game : Work for 1 client
    
    * Chat_in_game : add font
    
    * chat_in_game : /rename command and display smoother
    
    * Chat_in game : Progression for send chat message between clients
    
    * Chat_in_game : works most of a time
    
    * Chat_in_game : Documentation
    
    * feat(chat) : utf8 no more asc
    
    Co-authored-by: bgaro <b.garo44@gmail.com>

---
+++ src/UI/chat.py
@@ -0,0 +1,79 @@
+from os import path
+
+from pygame import color
+
+from src.config.colors import GREY
+from .log import Log
+from src.interface.Input import Input
+import pygame as pg
+from src.config.assets import fonts_folder
+
+
+class Chat:
+    def __init__(self, width, height, position, text_color, font_size, game):
+        self.width = width
+        self.height = height
+        self.position = position
+        self.text_color = text_color
+        self.font_size = font_size
+        self.game = game
+        self.log = Log(width, height, position,
+                       self.text_color, font_size, game)
+        self.my_inputbox = Input(
+            game, position[1] + width // 2, position[0] + height + 50, width, 50, path.join(fonts_folder, 'ShareTechMono-Regular.ttf'), font_size, max_length=(width // font_size))
+        self.user_text = ""
+        self.is_send = False
+        self.text_to_send = ""
+        self.name = "You "
+
+    def send_chat(self):
+        """ Sending data trought the send methode of client
+        """
+        self.game.client.send(str(self.game.own_id)
+                              + " 8 " + self.user_text, chat=True)
+
+    def receive_chat(self, message):
+        """[summary]
+
+        Args:
+            message (string): message that the client receive trought the network
+        """
+        message_to_print = message.replace("_", " ")
+        self.log.add_log(message_to_print)
+
+    def event_handler(self, event):
+        """event manager
+
+
+        """
+
+        if event.type == pg.KEYDOWN:
+
+            if event.key == pg.K_RETURN and self.my_inputbox.get_text() != "":
+                self.user_text = self.my_inputbox.get_text()
+                if self.user_text[0] == "/":
+                    first_word = self.user_text.split()[0]
+                    second_word = self.user_text.split()[1]
+                    if first_word == "/rename":
+                        self.name = second_word
+                    if first_word == "/color":
+                        self.text_color = second_word
+
+                if self.user_text[0] != "/":
+                    self.log.add_log(self.name + " : "
+                                     + self.user_text, self.text_color)
+                    self.send_chat()
+
+                self.my_inputbox.flush_text()
+
+        self.my_inputbox.handle_events(event)
+        self.log.event(event)
+
+    def draw(self, surface):
+        """drawing of log and input bpx 
+
+        Args:
+            surface : Surface where will be draw the log and the input
+        """
+        self.log.print_log(surface)
+        self.my_inputbox.display_box()
--- src/config/colors.py
+++ src/config/colors.py
@@ -4,3 +4,4 @@
 BLACK = "#000000"
 WHITE = "#ffffff"
 BROWN = "#A52A2A"
+GREY = "#737679"
