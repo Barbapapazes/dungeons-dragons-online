589baf231d4ac166f8bc520352362248dace095e
Makelonn <71496589+Makelonn@users.noreply.github.com>
2021-04-16 03:15:33 -0400

    Player (#11)
    * feat(map) : added graphics and did minor changes
    
    * feature/map : basic map function
    
    * feature/map : display map
    
    * build(maps) : rearranged files
    
    * build(maps) : fixed settings files import
    
    * feat(player): added class player, can move with zqsd
    
    * feat(minimap): display a minimap + did some cleaning
    
    * Create pathfinder, not fully working
    
    * feat(player): added pathfinder
    
    * feat(player): manage health & created stats

--- src/Map.py
+++ src/Map.py
@@ -1,4 +1,5 @@
 from src.config.window import TILE_SIZE, RESOLUTION
+from src.config.colors import DARKBROWN
 import pygame as pg
 from random import randint
 
@@ -53,46 +54,6 @@ class Tile:
         self.discovered = True
 
 
-"""
-class Doors:
-    "Define doors (portals) in maps"
-
-    def __init__(self, side1, map1, side2, map2):
-        self.side1 = side1  # coordonate of a side of the door in tiles
-        self.map1 = map1  # map of a side of a door
-        self.side2 = side2  # coordonate of a side of the other door in tiles
-        self.map2 = map2  # map of the otehr side of the other door
-
-    def is_inside(self, mapid, x, y, mx, my, player_list, map):
-        "check if a player is inside the door and clicking on it. If so switch the map"
-        # check side 1
-        if mapid == self.map1 and all(
-            (self.side1[0] * TILE_SIZE <= px < (self.side1[0] + 1) * TILE_SIZE)
-            and (
-                self.side1[1] * TILE_SIZE
-                <= py
-                < (self.side1[1] + 1) * TILE_SIZE
-            )
-            for px, py in [[x, y], [mx, my]]
-        ):
-            map.switch(self.map2, self.side2, player_list)
-            return True
-        # check the side 2
-        if mapid == self.map2 and all(
-            (self.side2[0] * TILE_SIZE <= px < (self.side2[0] + 1) * TILE_SIZE)
-            and (
-                self.side2[1] * TILE_SIZE
-                <= py
-                < (self.side2[1] + 1) * TILE_SIZE
-            )
-            for px, py in [[x, y], [mx, my]]
-        ):
-            map.switch(self.map1, self.side1, player_list)
-            return True
-        return False
-"""
-
-
 class Map:
     "Define maps for the current game"
 
@@ -103,7 +64,7 @@ class Map:
         self.name = map_lines[0][:-1]
         # Note : self.map contain all map sorted as [dungeon number][floor number][Y][X]
         # X is the position in the Y line
-        main_map = [
+        self.map = [
             [
                 Tile(map_lines[lineY][placeX])
                 for placeX in range(len(map_lines[lineY][:-1]))
@@ -115,29 +76,101 @@ class Map:
             [int(map_lines[i][:-1]), int(map_lines[i + 1][:-1])]
             for i in range(-12, 0, 2)
         ]
-        self.map = main_map
+        self.centered_in = [2, 2]  # X and Y where is centered
         ### Setting minimap ###
-        # Not done yet
-        self.init_views()
+        minimap_reduction = 8
+        self.s_width, self.s_height = RESOLUTION
+        self.nb_tileX, self.nb_tileY = (
+            self.s_width // TILE_SIZE) - 1, (self.s_height // TILE_SIZE) - 1
+        self.minimap_size = self.s_width // minimap_reduction, self.s_height // minimap_reduction
+        self.minimap = pg.Surface(self.minimap_size, minimap_reduction)
+        ###Setting canvas###
+        self.init_views(self.minimap_size, minimap_reduction)
 
-    def init_views(self):
+    def init_views(self, mini_size, mini_coeff):
         """Init array of every surface to blit on the screen (tiles & vision)"""
         lenX, lenY = len(self.map[0]), len(self.map)
         display = pg.Surface((lenX * TILE_SIZE, lenY * TILE_SIZE))
+        self.mini_tile_s = TILE_SIZE // mini_coeff
+        mini_display = pg.Surface(
+            (lenX * self.mini_tile_s, lenY * self.mini_tile_s))
         for tileY in range(lenY):
             for tileX in range(lenX):
                 display.blit(
                     self.map[tileY][tileX].image,
                     (tileX * TILE_SIZE, tileY * TILE_SIZE),
                 )
+                ### Minimap ###
+                # Transform image in a mini image
+                mini_img = pg.Surface.copy(self.map[tileY][tileX].image)
+                pg.transform.scale(
+                    mini_img, (self.mini_tile_s, self.mini_tile_s))
+
+                mini_display.blit(
+                    mini_img, (tileX * self.mini_tile_s, tileY * self.mini_tile_s))
         self.map_canva = display
+        self.mini_canva = mini_display
         # self.mini_canva_list = []
         # add view list here
 
-    def draw(self, display, x, y):
+    def draw(self, display):
         "draw a part of map center in x,y (x and y being couted in tiles"
-        s_width, s_height = RESOLUTION
         display.blit(
             self.map_canva,
-            (-x * TILE_SIZE + s_width / 2, -y * TILE_SIZE + s_height / 2),
+            (-self.centered_in[0] * TILE_SIZE
+             + self.s_width / 2, -self.centered_in[1] * TILE_SIZE + self.s_height / 2),
         )  # display the carpet at the right place
+
+    def draw_mini(self, display):
+        "Display a minimap"
+        self.minimap.fill(DARKBROWN)
+        self.minimap.blit(self.mini_canva, (-self.centered_in[0] * self.mini_tile_s
+                                            + self.minimap_size[0] / 2, -self.centered_in[1] * self.mini_tile_s + self.minimap_size[1] / 2))
+        display.blit(self.minimap, (self.s_width
+                                    - self.minimap_size[0] - 4, 4))
+
+    def get_clicked_tile(self):
+        """Return the clicked tile"""
+        m_posX, m_posY = pg.mouse.get_pos()
+        m_posX = self.centered_in[0] - \
+            (self.nb_tileX // 2) + (m_posX // TILE_SIZE) - 1
+        m_posY = self.centered_in[1] - \
+            (self.nb_tileY // 2) + (m_posY // TILE_SIZE) - 1
+        return((m_posX, m_posY))
+
+    def is_valid_tile(self, numX, numY):
+        """Return if a tile is a valid number on the map"""
+        return ((0 <= numX < len(self.map[1])) and (0 <= numY < len(self.map)))
+
+    def simple_neigh(self, X, Y):
+        """Return list of around tile, not minding void and wall"""
+        return (('UL', (X - 1, Y - 1)), ('UR', (X + 1, Y - 1)),
+                ('DL', (X - 1, Y + 1)), ('DR', (X + 1, Y + 1)),
+                ('U', (X, Y - 1)), ('D', (X, Y + 1)),
+                ('L', (X - 1, Y)), ('R', (X + 1, Y)))
+
+    def neighbor_list(self, X, Y):
+        """ Return list of accessible neighbors for tile (X,Y) and directions"""
+        around = []
+        corner_list = {'UL': (X - 1, Y - 1), 'UR': (X + 1, Y - 1),
+                       'DL': (X - 1, Y + 1), 'DR': (X + 1, Y + 1)}
+        side_list = {'U': (X, Y - 1), 'D': (X, Y + 1),
+                     'L': (X - 1, Y), 'R': (X + 1, Y)}
+        for action, tile in side_list.items():
+            if not self.map[tile[1]][tile[0]].wall:
+                around.append((tile, action))
+
+        # + tester si DL UL UR DR sont des murs
+        if not self.map[side_list['U'][1]][side_list['U'][0]].wall:
+            if not self.map[side_list['L'][1]][side_list['L'][0]].wall:
+                around.append((corner_list['UL'], 'UL'))
+            if not self.map[side_list['R'][1]][side_list['R'][0]].wall:
+                around.append((corner_list['UR'], 'UR'))
+
+        if not self.map[side_list['D'][1]][side_list['D'][0]].wall:
+            if not self.map[side_list['L'][1]][side_list['L'][0]].wall:
+                around.append((corner_list['DL'], 'DL'))
+            if not self.map[side_list['R'][1]][side_list['R'][0]].wall:
+                around.append((corner_list['DR'], 'DR'))
+
+        return around
