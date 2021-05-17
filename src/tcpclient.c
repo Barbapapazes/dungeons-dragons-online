91511dfe94ef41c3ad63aa992c346d3820cee63a
bgaro <b.garo44@gmail.com>
2021-05-17 22:05:38 +0200

    test

--- src/tcpclient.c
+++ src/tcpclient.c
@@ -33,36 +33,14 @@ void end(int signum)
     exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char *argv[])
+int connection(char *ip, char *port)
 {
-    signal(SIGUSR1, &end); // signal handler : catch SIGUSR1 and execute end function
-
-    /* Buffer */
-    char *stdin_read = NULL;
-    char buffer[80];
-
-    /* Socket */
+    printf("connection _%s_ _%s_ \n", ip, port);
     int sockfd = socket(PF_INET, SOCK_STREAM, 0); //create a new tcp socket
-    size_t size;
-    int res, opt;
-
-    /* Ping */
     struct timeval timeout;
-    struct timeval ping_in, ping_out;
     timeout.tv_sec = 0.1;
     timeout.tv_usec = 0;
-
-    /* Packet */
-    unsigned char *s_packet;
-    game_packet game_data = {0, -1, ""}; // initialize a game_packet structure that will contain all the needed information
-    int packet_length = 0;
-    char *end; //pointer use in string to int conversion
-
-    if (sockfd < 0)
-        stop("socket()");
-
-    if (argc != 3)
-        stop("arg");
+    int res, opt;
 
     // prepare the server struct
     struct sockaddr_in serv_addr;
@@ -70,11 +48,11 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET; //server address : ipv4
 
     // set server adress
-    if (inet_aton(argv[1], &serv_addr.sin_addr) == 0)
+    if (inet_aton(ip, &serv_addr.sin_addr) == 0)
         stop("inet_aton()");
 
     // set server port
-    serv_addr.sin_port = htons(atoi(argv[2]));
+    serv_addr.sin_port = htons(atoi(port));
 
     // get socket flags
     if ((opt = fcntl(sockfd, F_GETFL, NULL)) < 0)
@@ -103,17 +81,17 @@ int main(int argc, char *argv[])
 
     // reset socket flags
     if (fcntl(sockfd, F_SETFL, opt) < 0)
-        stop("connect");
+        stop("socket flags");
 
     // if error occured in connect or select
     if (res < 0)
-        stop("connect");
+        stop("connect or select");
 
     // if timed out
     else if (res == 0)
     {
         errno = ETIMEDOUT;
-        stop("connect");
+        stop("timedout");
     }
     else
     {
@@ -121,15 +99,41 @@ int main(int argc, char *argv[])
 
         // check for errors in socket layer
         if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &opt, &len) < 0)
-            stop("connect");
+            stop("socket layer");
 
         // if error
         if (opt)
         {
             errno = opt;
-            stop("connect");
+            printf("test\n");
+            printf("%d\n", opt);
+            stop("opt");
         }
     }
+    return sockfd;
+}
+
+int main(int argc, char *argv[])
+{
+    signal(SIGUSR1, &end); // signal handler : catch SIGUSR1 and execute end function
+
+    /* Buffer */
+    char *stdin_read = NULL;
+    char buffer[80];
+
+    /* Socket */
+    int socket_list[2000];
+    int current_socket = 0;
+    size_t size;
+
+    /* Ping */
+    struct timeval ping_in, ping_out;
+
+    /* Packet */
+    unsigned char *s_packet;
+    game_packet game_data = {0, -1, ""}; // initialize a game_packet structure that will contain all the needed information
+    int packet_length = 0;
+    char *end; //pointer use in string to int conversion
 
     while (1)
     {
@@ -137,39 +141,50 @@ int main(int argc, char *argv[])
         // the python program will send data by line block
         if (getline(&stdin_read, &size, stdin) == -1) //get a line on stdin (BLOCKING function)
             stop("getline()");
+        if (stdin_read[0] == '!')
+        {
+            stdin_read++;
+            char *ip = strtok(stdin_read, " ");
+            char *port = strtok(NULL, " ");
+            port[strlen(port) - 1] = '\0';
+            socket_list[current_socket] = connection(ip, port);
+            current_socket++;
+        }
+        // else
+        // {
+        //     game_data.player_id = strtol(stdin_read, &end, 10);
 
-        game_data.player_id = strtol(stdin_read, &end, 10);
+        //     if (getline(&stdin_read, &size, stdin) == -1) //get a line on stdin (BLOCKING function)
+        //         stop("getline()");
 
-        if (getline(&stdin_read, &size, stdin) == -1) //get a line on stdin (BLOCKING function)
-            stop("getline()");
+        //     game_data.action = strtol(stdin_read, &end, 10);
 
-        game_data.action = strtol(stdin_read, &end, 10);
+        //     if (getline(&stdin_read, &size, stdin) == -1) //get a line on stdin (BLOCKING function)
+        //         stop("getline()");
 
-        if (getline(&stdin_read, &size, stdin) == -1) //get a line on stdin (BLOCKING function)
-            stop("getline()");
+        //     game_data.data = strdup(stdin_read); //duplicate so the data won't have the same adress as stdin_read and we can modify it
 
-        game_data.data = strdup(stdin_read); //duplicate so the data won't have the same adress as stdin_read and we can modify it
+        //     // format packet
+        //     s_packet = serialize_packet(game_data);
+        //     packet_length = 3 * sizeof(int) + strlen(game_data.data); // check serialization for more details
 
-        // format packet
-        s_packet = serialize_packet(game_data);
-        packet_length = 3 * sizeof(int) + strlen(game_data.data); // check serialization for more details
+        //     if (send(sockfd, s_packet, packet_length, 0) == -1) // send the message
+        //         stop("send()");
 
-        if (send(sockfd, s_packet, packet_length, 0) == -1) // send the message
-            stop("send()");
+        //     gettimeofday(&ping_in, NULL); // get time in s and µs when the packet has been sent
 
-        gettimeofday(&ping_in, NULL); // get time in s and µs when the packet has been sent
+        //     if (recv(sockfd, buffer, 2, 0) == -1) //wait for a server response
+        //         stop("send()");
 
-        if (recv(sockfd, buffer, 2, 0) == -1) //wait for a server response
-            stop("send()");
-
-        // get time in s and µs when the packet has been received
-        gettimeofday(&ping_out, NULL);
-        sprintf(buffer, "ping : %lu us\n", (ping_out.tv_sec - ping_in.tv_sec) * 1000000 + ping_out.tv_usec - ping_in.tv_usec);
-        write(STDOUT_FILENO, buffer, strlen(buffer));
+        //     // get time in s and µs when the packet has been received
+        //     gettimeofday(&ping_out, NULL);
+        //     sprintf(buffer, "ping : %lu us\n", (ping_out.tv_sec - ping_in.tv_sec) * 1000000 + ping_out.tv_usec - ping_in.tv_usec);
+        //     write(STDOUT_FILENO, buffer, strlen(buffer));
+        // }
     }
 
-    if (close(sockfd) == -1) // close file descriptor
-        stop("close");
+    // if (close(sockfd) == -1) // close file descriptor
+    //     stop("close");
 
     exit(EXIT_SUCCESS);
 }
